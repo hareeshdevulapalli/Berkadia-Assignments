e0e189d807c49f7e1ad6220547e6c25218a00ec8
Hareesh Devulapalli <44803046+hareeshdevulapalli@users.noreply.github.com>
2018-12-26 21:07:17 +0530

    Update aq.service.ts

--- week7/Angular/AirQuality/src/app/aq.service.ts
+++ week7/Angular/AirQuality/src/app/aq.service.ts
@@ -7,11 +7,11 @@ import {HttpClient} from '@angular/common/http';
 export class AqService {
   countryurl;
   cityurl;
-  localurl;
+  locationurl;
   constructor(private readonly http:HttpClient) {
     this.countryurl='https://api.openaq.org/v1/countries?sort=asc';
     this.cityurl='https://api.openaq.org/v1/latest?';
-    this.localurl='https://api.openaq.org/v1/latest?location='
+    this.locationurl='https://api.openaq.org/v1/latest?location='
    }
    getCoutries(){
      return this.http.get(this.countryurl);
@@ -19,7 +19,7 @@ export class AqService {
    getCityDetails(city){
      return this.http.get(this.cityurl+'city='+city);
    }
-   getLocationd(loca){
-     return this.http.get(this.localurl+loca);
+   getLocationd(location){
+     return this.http.get(this.locationurl+location);
    }
 }
